1a624a4257a01f57b1c5f56ee3e98ac493126665
FaustPipeDream <Indexalice@163.com>
2018-06-18 19:59:55 +0800

    Initial commit

---
+++ src/main/java/com/forum/myforum/MyforumApplication.java
@@ -0,0 +1,12 @@
+package com.forum.myforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyforumApplication.class, args);
+	}
+}
